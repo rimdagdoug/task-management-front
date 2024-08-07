5ef8cee42d5300282effe3edf520284eb06fcde9
rim <rimdagdoug99@gmail.com>
2024-08-07 12:58:33 +0200

    feature: initialize project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import TasksPage from './pages/TasksPage.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <TasksPage></TasksPage>
+  <Footer></Footer>
+</template>
+
+<style scoped>
+
+</style>
