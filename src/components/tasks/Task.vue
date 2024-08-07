f85b4f5b9808fa312f7840c547b979c6e55d565a
rim <rimdagdoug99@gmail.com>
2024-08-08 01:13:38 +0200

    Creating an Inline editing feature

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -2,32 +2,33 @@
     <li class="list-group-item py-3">
         <div class="d-flex justify-content-start align-items-center">
             <input class="form-check-input mt-0" :class="completedClass" type="checkbox" :checked="task.is_completed" />
-            <div class="ms-2 flex-grow-1" :class="completedClass" title="Double click the text to edit or remove">
-                <!-- <div class="relative">
-                    <input class="editable-task" type="text" />
-                </div> -->
-                <span>{{task.name}}</span>
+            <div class="ms-2 flex-grow-1" 
+            :class="completedClass"
+             title="Double click the text to edit or remove"
+             @dblclick="$event => isEdit = true"
+             >
+                <div class="relative" v-if="isEdit">
+                    <input class="editable-task" type="text" @keyup.esc="$event => isEdit = false" />
+                </div>
+                <span v-else>{{task.name}}</span>
             </div>
             <!-- <div class="task-date">24 Feb 12:00</div> -->
         </div>
-        <div class="task-actions">
-            <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
-                <IconPencil></IconPencil>
-            </button>
-            <button class="btn btn-sm btn-circle btn-outline-danger">
-                <IconTrash></IconTrash>
-            </button>
-        </div>
+        <TaskAction @edit="$event => isEdit = true" v-show="!isEdit" v-focus></TaskAction>
     </li>
 </template>
 
 <script setup>
-import IconPencil from '../icons/IconPencil.vue';
-import IconTrash from '../icons/IconTrash.vue';
-import { computed } from 'vue';
+import TaskAction from './TaskAction.vue';
+import { computed, ref } from 'vue';
 const props = defineProps({
     task: Object 
 })
 
+const isEdit = ref(false)
 const completedClass = computed( () => props.task.is_completed ? "completed" : "" )
+
+const vFocus = {
+    mounted: (el) => el.focus()
+}
 </script>
