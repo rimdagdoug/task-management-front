53fb3aa56efb51a66e590566f5a2b85c87af89c8
rim <rimdagdoug99@gmail.com>
2024-08-12 12:37:34 +0200

    feature:add transitions

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,21 @@ import Footer from './components/Footer.vue';
 
 <template>
   <Navbar></Navbar>
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <Footer></Footer>
 </template>
 
 <style scoped>
-
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
