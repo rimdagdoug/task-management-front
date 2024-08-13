b022cfa9e637603cdae474c820a6ad10eec06b59
rim <rimdagdoug99@gmail.com>
2024-08-13 09:50:29 +0200

    feature:Add Storing Axios Base URL in environment variables

--- src/components/tasks/Tasks.vue
+++ src/components/tasks/Tasks.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card mt-2" v-if="tasks.length && show">
+    <div class="card mt-2" v-if="tasks?.length && show">
         <ul class="list-group list-group-flush">
            <Task  v-for="task in tasks"
             :task="task" :key="task.id"
